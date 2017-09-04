55a88da7ca11a218d062e80d0504624b9de72685
Yohan Boniface <yb@enix.org>
2017-09-05 00:01:38 +0200

    Remove leading slash from bench
    Should we enforce this by removing "/" at insert time (unless it's
    the only char)?

--- tools/bench.py
+++ tools/bench.py
@@ -1,37 +1,37 @@
 from timeit import timeit
 from autoroutes import Routes, NoRoute
 
-PATHS = ['/user/', '/user/{id}', '/user/{id}/subpath', '/user/{id}/subpath2',
-         '/boat/', '/boat/{id}', '/boat/{id}/subpath', '/boat/{id}/subpath2',
-         '/horse/', '/horse/{id}', '/horse/{id}/subpath',
-         '/horse/{id}/subpath2', '/bicycle/', '/bicycle/{id}',
-         '/bicycle/{id}/subpath2', '/bicycle/{id}/subpath',
-         '/{foo}/{bar}/{baz}/{last}', '/api/{bar}/{baz}/{last}', '/api/id']
+PATHS = ['user/', 'user/{id}', 'user/{id}/subpath', 'user/{id}/subpath2',
+         'boat/', 'boat/{id}', 'boat/{id}/subpath', 'boat/{id}/subpath2',
+         'horse/', 'horse/{id}', 'horse/{id}/subpath',
+         'horse/{id}/subpath2', 'bicycle/', 'bicycle/{id}',
+         'bicycle/{id}/subpath2', 'bicycle/{id}/subpath',
+         '{foo}/{bar}/{baz}/{last}', 'api/{bar}/{baz}/{last}', 'api/id']
 
 routes = Routes()
 for i, path in enumerate(PATHS):
     routes.connect(path.encode(), GET=i)
 
-node = routes.follow(b'/user/')
-print('/user/', node)
-node = routes.follow(b'/horse/22/subpath')
-print('/horse/22/subpath', node)
-node = routes.follow(b'/api/id/baz/last')
-print('/api/id/baz/last', node)
+node = routes.follow(b'user/')
+print('user/', node)
+node = routes.follow(b'horse/22/subpath')
+print('horse/22/subpath', node)
+node = routes.follow(b'api/id/baz/last')
+print('api/id/baz/last', node)
 try:
-    routes.follow(b'/plane/')
+    routes.follow(b'plane/')
 except NoRoute:
-    print('/plane/ not found')
+    print('plane/ not found')
 else:
     print('Oops, not raised')
 
-total = timeit("routes.follow(b'/user/')", globals=globals(), number=100000)
+total = timeit("routes.follow(b'user/')", globals=globals(), number=100000)
 print(f'First flat path:\n> {total}')
 
-total = timeit("routes.follow(b'/horse/22/subpath')", globals=globals(),
+total = timeit("routes.follow(b'horse/22/subpath')", globals=globals(),
                number=100000)
 print(f'Middle path with placeholder:\n> {total}')
 
-total = timeit("try:\n routes.follow(b'/plane/')\nexcept NoRoute:\n pass",
+total = timeit("try:\n routes.follow(b'plane/')\nexcept NoRoute:\n pass",
                globals=globals(), number=100000)
 print(f'Not found path:\n> {total}')
